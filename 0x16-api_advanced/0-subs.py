5a5aa3277446e7a3096619790ed4e62c7d66afec
AMahadai <somebiz777@gmail.com>
2024-09-26 11:15:34 -0700

    Handled errors on task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,9 +10,18 @@ def number_of_subscribers(subreddit):
     headers = {'user-agent': 'My user agent'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subs = data.get('data').get('subscribers')
-        return subs
-    else:
-        return(0)
+    try:
+        if response.status_code == 200:
+            data = response.json()
+            subs = data.get('data').get('subscribers')
+            return subs
+        elif response.status_code == 429:
+            # handle rate limit
+            print('Rate limited, try again later')
+        elif response.status_code == 403:
+            # handle forbidden
+            print(f'HTTP Status code: {response.status_code}')
+    except requests.RequestException as e:
+        print(f'Error occured: {e}')
+
+    return 0
