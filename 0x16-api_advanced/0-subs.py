7d2c1bcd8c28b5516c38345036d0847fb920d2ac
AMahadai <somebiz777@gmail.com>
2024-09-26 10:26:48 -0700

    edited 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-"""
-    Uses Reddit API to print the number of subscribers of a subreddit
-"""
+""" A script to fetch number of subscribers"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Get the number of subscribers for a given subreddit
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'user-agent': 'request'}
+    """Read reddit API and return number subscribers """
+
+    headers = {'user-agent': 'My user agent'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
-        return 0
-
-    data = response.json().get("data")
-    num_subs = data.get("subscribers")
-
-    return num_subs
+    if response.status_code == 200:
+        data = response.json()
+        subs = data.get('data').get('subscribers')
+        return subs
+    else:
+        return(0)
